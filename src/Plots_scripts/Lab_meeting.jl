1a9d7814cdc485b8c8f0dd2ca3efd2421e61b115
DarioSarra <dariosarra@gmail.com>
2020-05-24 18:31:37 +0100

    presented at lab meeting

--- src/Plots_scripts/Lab_meeting.jl
+++ src/Plots_scripts/Lab_meeting.jl
@@ -9,13 +9,15 @@ list = ["PreVehicle",
     "SB242084_opt",
     "Saline"]
 s = filter(r->r.Treatment in list &&
-    r.Trial < 61 &&
+    r.Trial < 31 &&
+    # r.Trial_Travel_to < 40 &&
     r.MouseID != "pc7",
     streaks)
 s[s.Treatment .== "Saline",:Treatment] = [o ? "Optogenetic" : "PreVehicle" for o in s[s.Treatment .== "Saline",:Stim]]
 s[s.Treatment .== "SB242084_opt",:Phase] .=  "SB242084_opt"
 s[s.Treatment .== "SB242084_opt",:Treatment] = [o ? "SB242084_opt" : "PreVehicle" for o in s[s.Treatment .== "SB242084_opt",:Stim]]
 # Df = combine([:Treatment,:Stim_Day] => (t,s) -> (treatment = union(t)), groupby(s,:Phase))
+######################## All  Manipulations ###################################
 df1 = combine(groupby(s,:Phase)) do dd
     subdf = unstack(dd,:Treatment,:Num_pokes)
     current_drug = Symbol(subdf[1,:Phase])
@@ -26,37 +28,11 @@ df2 = combine(groupby(df1,[:Phase])) do dd
 end
 Drug_colors!(df2)
 plot_wilcoxon(df2)
-# savefig(joinpath(figs_loc,"Fig3/WilcoxonNumPokes.pdf"))
 ##
-check = filter(r-> occursin("pc",r.MouseID), s)
-c = combine([:Stim_Day,:Stim] => (d,s) -> (stimday = union(d),stim = [length(union(s))]), groupby(check,[:MouseID,:Day]))
-open_html_table(c)
+savefig(joinpath(figs_loc,"LabMeeting/WilcoxonNumPokes30.pdf"))
 ##
-union(s.Phase)
-Df = combine(groupby(s,[:Phase,:Treatment])) do dd
-    ecdf(dd,:Num_pokes; mode = :conf)
-end
-# open_html_table(Df)
-Drug_colors!(Df)
-gd = groupby(Df,:Phase)
-tp = [@df subdf plot(:Xaxis, :Mean,
-    xlabel = :Phase[1],
-    xticks = 0:10:60,
-    xrotation = 30,
-    group = :Treatment,
-    linecolor = :color,
-    legend = false,
-    ribbon = :ERR,
-    linewidth = 2,
-    fillalpha = 0.3) for subdf in gd]
-ord = [6,1,2,5,3,4]
-pp =  plot(tp[ord]...)
-##
-plot(tp[6])
-
-## Traveltime analysis
 df1 = combine(groupby(s,:Phase)) do dd
-    subdf = unstack(dd,:Treatment,:Travel_to)
+    subdf = unstack(dd,:Treatment,:Trial_Travel_to)
     current_drug = Symbol(subdf[1,:Phase])
     rename!(subdf, current_drug => :Drug)
 end
@@ -66,13 +42,141 @@ end
 Drug_colors!(df2)
 plot_wilcoxon(df2)
 ##
-check = filter(r-> occursin("pc",r.MouseID), streaks)
-println.(union(streaks[:,:MouseID]))
-union(check[:,:Stim_Day])
-union(check[:,:Stim])
-
-df1 = combine(:Num_pokes => mean,groupby(check,[:Treatment,:Stim,:MouseID]))
-open_html_table(df1)
-
-df2 = unstack(df1,:Stim,:Num_pokes_mean)
-open_html_table(df2)
+savefig(joinpath(figs_loc,"LabMeeting/WilcoxonTravelROI30.pdf"))
+##
+df1 = combine(groupby(s,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:AfterLast)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/WilcoxonAfterLast30.pdf"))
+######################## 5HT Release  Manipulations ###################################
+release_man =  filter(r -> r.Phase in ["Citalopram", "Optogenetic"], s)
+df1 = combine(groupby(release_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:Num_pokes)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/ReleaseNumPokes30.pdf"))
+##
+df1 = combine(groupby(release_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:Trial_Travel_to)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/ReleaseTravelROI30.pdf"))
+##
+df1 = combine(groupby(release_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:AfterLast)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/ReleaseAfterLast30.pdf"))
+######################## Global  Manipulations ###################################
+glob_man =  filter(r -> r.Phase in ["Citalopram", "Optogenetic", "Methysergide"], s)
+df1 = combine(groupby(glob_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:Num_pokes)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/GlobalNumPokes30.pdf"))
+##
+df1 = combine(groupby(glob_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:Trial_Travel_to)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/GlobalTravelROI30.pdf"))
+##
+df1 = combine(groupby(glob_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:AfterLast)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/GlobalAfterLast30.pdf"))
+######################## Selective  Manipulations ###################################
+sel_man =  filter(r -> r.Phase in ["SB242084", "Altanserin", "Way_100135"], s)
+df1 = combine(groupby(sel_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:Num_pokes)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/SelectiveNumPokes30.pdf"))
+##
+df1 = combine(groupby(sel_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:Trial_Travel_to)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/SelectiveTravelROI30.pdf"))
+##
+df1 = combine(groupby(sel_man,:Phase)) do dd
+    subdf = unstack(dd,:Treatment,:AfterLast)
+    current_drug = Symbol(subdf[1,:Phase])
+    rename!(subdf, current_drug => :Drug)
+end
+df2 = combine(groupby(df1,[:Phase])) do dd
+    wilcoxon(dd,:Drug, :PreVehicle; f = x -> mean(skipmissing(x)))
+end
+Drug_colors!(df2)
+plot_wilcoxon(df2)
+##
+savefig(joinpath(figs_loc,"LabMeeting/SelectiveAfterLast30.pdf"))
+##
+x = collect(1:100)
+y = -log.(10,x)
+plot(x,y)
